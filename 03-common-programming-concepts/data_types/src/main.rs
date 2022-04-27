96fdbcd550c2534eb120a3e30e37e36017f5f031
Ved Kothavade <kothavade.ved@gmail.com>
2022-04-27 15:42:12 -0400

    Update 03

---
+++ 03-common-programming-concepts/data_types/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3;
+    let remainder = 43 % 5;
+
+    let t = true;
+    let f: bool = false;
+
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
+
+    let a = [1, 2, 3, 4, 5];
+    let b: [i32; 5] = [1, 2, 3, 4, 5];
+    let c = [3; 5]; //[3, 3, 3, 3, 3]
+}
